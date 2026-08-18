356a5d2843bc836046edc18c11f3c35dda170933
Ali Emre Özcan <aliemrozcan@gmail.com>
2026-08-18 15:07:51 +0300

    config: VAPID acik anahtari

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ self.HN_CONFIG = {
   WORKER_URL: "",
 
   // tools/vapid.html ile urettigin ACIK anahtar (base64url, ~87 karakter).
-  VAPID_PUBLIC_KEY: "",
+  VAPID_PUBLIC_KEY: "BCfLeqQpigEhfz7QSO_LosCk8M8KoXlkgi7m5BgI-ZdsW4IwgZ1kQvaT7yuVGQey8td7_d41HkqhrFqUU_Wbr4w",
 
   // Varsayilan hatirlatma saatleri (kullanici Ayarlar'dan degistirebilir)
   DEFAULT_TIMES: ["08:30", "20:30"],
